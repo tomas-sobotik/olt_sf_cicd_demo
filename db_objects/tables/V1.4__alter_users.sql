c74f1a790d62b227f30a3ade247a0d41514d5557
Tomas Sobotik <tomas.sobotik@tieto.com>
2024-01-23 11:53:34 +0100

    task xyz

--- db_objects/tables/V1.4__alter_users.sql
+++ db_objects/tables/V1.4__alter_users.sql
@@ -1,11 +1,6 @@
+
 -- Set the database and schema context
 USE DATABASE {{ target_db }};
 USE SCHEMA PUBLIC;   
 
-CREATE OR REPLACE view users_view
-as select
-LAST_NAME,
-EMAIL,
-BIRTH_YEAR
-
-from users; 
+ALTER TABLE USERS add column test varchar;
