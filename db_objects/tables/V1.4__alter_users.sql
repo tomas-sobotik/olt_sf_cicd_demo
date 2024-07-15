9b680803909cc69d2c76383c001685b26144a772
Tomas Sobotik <tomas.sobotik@tieto.com>
2024-07-15 15:54:21 +0200

    alter

--- db_objects/tables/V1.4__alter_users.sql
+++ db_objects/tables/V1.4__alter_users.sql
@@ -1,11 +1,7 @@
+
+
 -- Set the database and schema context
 USE DATABASE {{ target_db }};
 USE SCHEMA PUBLIC;   
 
-CREATE OR REPLACE view users_view
-as select
-LAST_NAME,
-EMAIL,
-BIRTH_YEAR
-
-from users;   
+alter table users add column phone varchar;
