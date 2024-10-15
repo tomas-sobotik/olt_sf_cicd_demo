549eb843320b9c40e01b43e56ccff924d2f58e34
Tomas Sobotik <tomas.sobotik@tieto.com>
2024-10-15 15:57:20 +0200

    cleaning

--- db_objects/tables/V1.4__alter_users.sql
+++
@@ -1,6 +0,0 @@
-
--- Set the database and schema context
-USE DATABASE {{ target_db }};
-USE SCHEMA PUBLIC;   
-
-alter table users add column phone varchar; 
--- db_objects/tables/V1.5__alter_users.sql
+++
@@ -1,5 +0,0 @@
--- Set the database and schema context
-USE DATABASE {{ target_db }};
-USE SCHEMA PUBLIC;   
-
-ALTER TABLE USERS ADD COLUMN address varchar;
